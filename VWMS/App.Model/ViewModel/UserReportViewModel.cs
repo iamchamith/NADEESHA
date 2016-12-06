939fe84477746fcaf1b029c97b2851ad795f90d9
iamchamith <iamchamith@gmail.com>
2016-12-07 05:00:23 +0530

    add user report

---
+++ VWMS/App.Model/ViewModel/UserReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model.ViewModel
+{
+    public  class UserReportViewModel
+    {
+       
+        public string Email { get; set; }
+        public int? State { get; set; }
+        public string Name { get; set; }
+        public string Nic { get; set; }
+       
+    }
+}
